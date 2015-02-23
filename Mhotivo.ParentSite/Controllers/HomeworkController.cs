5adfad273810d5e1a08d718fe9cadc596b67fad7
siwady <siwady0908@unitec.edu>
2015-02-23 16:01:22 -0600

    Filtro por materia en el area de tareas

--- Mhotivo.ParentSite/Controllers/HomeworkController.cs
+++ Mhotivo.ParentSite/Controllers/HomeworkController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ using AutoMapper;
 using Mhotivo.Data.Entities;
 using Mhotivo.Interface.Interfaces;
 using Mhotivo.ParentSite.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace Mhotivo.ParentSite.Controllers
 {
@@ -32,17 +34,28 @@ namespace Mhotivo.ParentSite.Controllers
             _academicYearDetailRepository = academicYearDetailRepository;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string param)
         {
+            
             IEnumerable<Homework> allHomeworks = _homeworkRepository.GetAllHomeworks();
-
+            IEnumerable<Homework> allHomeworksWithFilter= _homeworkRepository.GetAllHomeworks();
+            if (!param.IsNullOrWhiteSpace())
+            {
+                //int Id = Convert.ToInt32(param);
+                //allHomeworksWithFilter =allHomeworks.Where(x => x.AcademicYearDetail.Course.Id == Id).ToList();
+            }
+            
             Mapper.CreateMap<HomeworkModel, Homework>().ReverseMap();
             IEnumerable<HomeworkModel> allHomeworksModel =
                 allHomeworks.Select(Mapper.Map<Homework, HomeworkModel>).ToList();
+
+            
 
                 
 
             return View(allHomeworksModel);
         }
+
+      
     }
 }
