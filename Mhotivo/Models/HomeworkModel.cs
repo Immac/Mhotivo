d88288951c3109037ffee04dccab0afb231e8791
Dennis Molina <dennismolina.17@gmail.com>
2015-03-02 12:50:21 -0600

    Agregado el rich text en el site de administracion al crear tareas

--- Mhotivo/Models/HomeworkModel.cs
+++ Mhotivo/Models/HomeworkModel.cs
@@ -1,4 +1,5 @@
-﻿using Mhotivo.Data.Entities;
+﻿using System.Web.Mvc;
+using Mhotivo.Data.Entities;
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -10,7 +11,7 @@ namespace Mhotivo.Models
 
         [Display(Name = "Título")]
         public string Title { get; set; }
-
+        [AllowHtml]
         [Display(Name = "Descripción")]
         public string Description { get; set; }
 
@@ -29,15 +30,20 @@ namespace Mhotivo.Models
 
     public class CreateHomeworkModel
     {
+        [Required(ErrorMessage = "Debe Ingresar título de la tarea.")]
         [Display(Name = "Título")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Debe Ingresar una descripción.")]
+        [AllowHtml]
         [Display(Name = "Descripción")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Debe Ingresar fecha de entrega.")]
         [Display(Name = "Día de entrega")]
         public string DeliverDate { get; set; }
 
+        [Required(ErrorMessage = "Debe Ingresar puntaje.")]
         [Display(Name = "Puntaje")]
         public float Points { get; set; }
 
@@ -48,20 +54,24 @@ namespace Mhotivo.Models
     public class EditHomeworkModel
     {
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Debe Ingresar título de la tarea.")]
         [Display(Name = "Titulo")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Debe Ingresar una descripción.")]
+        [AllowHtml]
         [Display(Name = "Descripcion")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Debe Ingresar fecha de entrega.")]
         [Display(Name = "Dia de entrega")]
         public string DeliverDate { get; set; }
 
+        [Required(ErrorMessage = "Debe Ingresar puntaje.")]
         [Display(Name = "puntaje")]
         public float Points { get; set; }
 
         [Display(Name = "Materia")]
-        public int course { get; set; }
+        public virtual AcademicYearDetail AcademicYearDetail { get; set; }
     }
 }
