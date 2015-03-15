daa328abd685eb24622301e6c8c6d20b5656d45c
Carlos Luis <cyber_heroe@unitec.edu>
2015-03-15 09:28:23 -0600

    Paginacion Students

--- Mhotivo/Controllers/StudentController.cs
+++ Mhotivo/Controllers/StudentController.cs
@@ -8,6 +8,7 @@ using Mhotivo.Data.Entities;
 using Mhotivo.Logic.ViewMessage;
 using Mhotivo.Models;
 using AutoMapper;
+using PagedList;
 
 namespace Mhotivo.Controllers
 {
@@ -27,7 +28,7 @@ namespace Mhotivo.Controllers
             _viewMessageLogic = new ViewMessageLogic(this);
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
             _viewMessageLogic.SetViewMessageIfExist();
             var allStudents = _studentRepository.GetAllStudents();
@@ -36,7 +37,46 @@ namespace Mhotivo.Controllers
             Mapper.CreateMap<DisplayStudentModel, Student>().ReverseMap();
             var allStudentDisplaysModel = allStudents.Select(Mapper.Map<Student, DisplayStudentModel>).ToList();
 
-            return View(allStudentDisplaysModel);
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            //if (page == null) throw new ArgumentNullException("page");
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                allStudentDisplaysModel = allStudentDisplaysModel.Where(s => s.FullName.Contains(searchString)).ToList();
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    allStudentDisplaysModel = allStudentDisplaysModel.OrderByDescending(s => s.FullName).ToList();
+                    break;
+                case "Date":
+                    allStudentDisplaysModel = allStudentDisplaysModel.OrderBy(s => s.StartDate).ToList();
+                    break;
+                case "date_desc":
+                    allStudentDisplaysModel = allStudentDisplaysModel.OrderByDescending(s => s.StartDate).ToList();
+                    break;
+                default:  // Name ascending 
+                    allStudentDisplaysModel = allStudentDisplaysModel.OrderBy(s => s.FullName).ToList();
+                    break;
+            }
+
+            const int pageSize = 10;
+            var pageNumber = (page ?? 1);
+            return View(allStudentDisplaysModel.ToPagedList(pageNumber, pageSize));
+            //return View(allStudentDisplaysModel);
         }
 
         [HttpGet]
